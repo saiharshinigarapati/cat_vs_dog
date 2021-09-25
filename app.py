9dc57c75a5a999ec93d2d266bd2f24aa0263264e
saiharshinigarapati <48383338+saiharshinigarapati@users.noreply.github.com>
2021-09-25 10:56:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,32 +10,24 @@ app = Flask(__name__)
 STATIC_FOLDER = 'static'
 # Path to the folder where we'll store the upload before prediction
 UPLOAD_FOLDER = STATIC_FOLDER + '/uploads'
+# Path to the folder where we store the different models
 MODEL_FOLDER = STATIC_FOLDER + '/models'
-
-global graph
-graph = tf.compat.v1.get_default_graph()
-
-    
-
-
 
 
 def load__model():
     """Load model once at running time for all the predictions"""
     print('[INFO] : Model loading ................')
-    
-    with graph.as_default():
-        #model = tf.keras.models.load_model(MODEL_FOLDER + '/catsVSdogs.h5')
-        global model
-
-        model = load_model(MODEL_FOLDER + '/cat_dog_classifier.h5')
-        print('[INFO] : Model loaded')
+    global model
+    # model = tf.keras.models.load_model(MODEL_FOLDER + '/catsVSdogs.h5')
+    model = load_model(MODEL_FOLDER + '/cat_dog_classifier.h5')
+    global graph
+    graph = tf.get_default_graph()
+    print('[INFO] : Model loaded')
 
 
 def predict(fullpath):
-    data = image.load_img(fullpath, target_size=(128,128,3))
+    data = image.load_img(fullpath, target_size=(128, 128, 3))
     # (150,150,3) ==> (1,150,150,3)
-    print(data)
     data = np.expand_dims(data, axis=0)
     # Scaling
     data = data.astype('float') / 255
@@ -43,7 +35,6 @@ def predict(fullpath):
     # Prediction
 
     with graph.as_default():
-
         result = model.predict(data)
 
     return result
@@ -66,6 +57,7 @@ def upload_file():
         file.save(fullname)
 
         result = predict(fullname)
+
         pred_prob = result.item()
 
         if pred_prob > .5:
